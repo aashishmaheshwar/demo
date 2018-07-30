3513146c9a812cbe0f74ad609634657fe1f0d13c
aashi <aashishmaheswar@gmail.com>
2018-07-30 22:04:15 +0530

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RangeSliderModule } from 'ngx-range-slider';
 
 import { AppComponent } from './app.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
@@ -14,7 +13,6 @@ import { SearchBoxComponent } from './search-box/search-box.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    RangeSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
