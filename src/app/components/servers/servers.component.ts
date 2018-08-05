d42ba0e3098a6e9f89aa74f87758faf69a361f86
aashi <aashishmaheswar@gmail.com>
2018-08-05 18:37:48 +0530

    fixing some styles

--- src/app/components/servers/servers.component.ts
+++ src/app/components/servers/servers.component.ts
@@ -30,16 +30,13 @@ export class ServersComponent implements OnInit {
     { key: 'SSD', selected: false }
   ];
   location_multiSelVals = [
-    { key: '2', selected: false },
-    { key: '4', selected: false },
-    { key: '8', selected: false },
-    { key: '12', selected: false },
-    { key: '16', selected: false },
-    { key: '24', selected: false },
-    { key: '32', selected: false },
-    { key: '48', selected: false },
-    { key: '64', selected: false },
-    { key: '96', selected: false }
+    { key: 'AmsterdamAMS-01', selected: false },
+    { key: 'Washington D.C.WDC-01', selected: false },
+    { key: 'San FranciscoSFO-12', selected: false },
+    { key: 'SingaporeSIN-11', selected: false },
+    { key: 'DallasDAL-10', selected: false },
+    { key: 'FrankfurtFRA-10', selected: false },
+    { key: 'Hong KongHKG-10', selected: false }
   ];
   // listVals = Observable.of([20, 32, 56, 77]);
   // create an interface ISearch where all are optional paramters.
