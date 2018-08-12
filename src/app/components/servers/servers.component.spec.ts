b25dd51b318f44cc93a81982c04332f14d45f4af
aashi <aashishmaheswar@gmail.com>
2018-08-12 19:41:57 +0530

    fixing broken test cases

--- src/app/components/servers/servers.component.spec.ts
+++ src/app/components/servers/servers.component.spec.ts
@@ -3,6 +3,9 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { ServersComponent } from "./servers.component";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ServerService } from "@services/server.service";
+import { HttpClientModule } from "@node_modules/@angular/common/http";
+import { SpinnerService } from "@services_utilities/spinner.service";
 
 describe("SearchBoxComponent", () => {
   let component: ServersComponent;
@@ -10,8 +13,10 @@ describe("SearchBoxComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ServersComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      providers: [ServerService, SpinnerService]
     }).compileComponents();
   }));
 
--- src/app/services/server.service.spec.ts
+++ src/app/services/server.service.spec.ts
@@ -1,15 +1,35 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from "@angular/core/testing";
 
-import { ServerService } from './server.service';
+import { ServerService } from "./server.service";
+import { HttpClientModule } from "@node_modules/@angular/common/http";
 
-describe('ServerService', () => {
+describe("ServerService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [ServerService]
     });
   });
 
-  it('should be created', inject([ServerService], (service: ServerService) => {
+  it("should be created", inject([ServerService], (service: ServerService) => {
     expect(service).toBeTruthy();
   }));
+
+  it("testing fake API call on static JSON data with params", inject(
+    [ServerService],
+    (service: ServerService) => {
+      let params = {};
+      params["ram"] = ["4", "8", "64"];
+      params["hdd"] = ["SATA2"];
+      params["location"] = ["AmsterdamAMS-01", "Washington D.C.WDC-01"];
+      params["storageMin"] = 0;
+      params["storageMax"] = 72000;
+      let serviceData = [];
+      service.getServers_StaticData(params).subscribe(data => {
+        serviceData = data;
+        return serviceData;
+      });
+      expect(serviceData["servers"].length).toEqual(66);
+    }
+  ));
 });
